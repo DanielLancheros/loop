27a481ea6314a7517118da75ef15e7fd57a9de37
Daniel Lancheros <daniellancheros@Daniels-MacBook-Air.local>
2023-06-13 15:59:08 -0400

    loops with arrays

---
+++ doitagain.js
@@ -0,0 +1,12 @@
+function arrayLoop(array) {
+
+for (let i = 0; i < array.length; i++ ) {
+    const result = array[i];
+    console.log(result);
+
+}
+
+}
+const cars = ["honda","toyota","ford","chevy","volkswagon","audi","bmw","ferrari","lamborghini","kia","nissan","fiat","lexus",];
+
+arrayLoop(cars);
---
+++ index.js
@@ -0,0 +1,27 @@
+function firstLoop(total) {
+
+for (let i = 0; i < total; i++) {
+    console.log(i);
+    }
+}
+function fingerLoop() {
+    const totalFinger = 10;
+
+for (let i = 0; i < totalFinger; i++) {
+    let sentence = 'I have '+ (i+1) +' fingers';
+    console.log(sentence);
+    }
+}
+
+function arrayLoop(array) {
+    
+for (let i = 0; i < prime.length; i++) {
+    const result = array[i];
+
+    console.log(result);
+
+}
+
+}
+const prime = [2,3,5,7,11,13,17,19,23,29,31,37,41,43,47];
+arrayLoop(prime);
